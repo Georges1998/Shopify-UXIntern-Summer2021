3253d933fe7a60208cc9b3430de2b6b14a1db418
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2020-12-16 18:57:57 -0500

    small fixes

--- src/app/material.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { MatSliderModule } from "@angular/material/slider";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-@NgModule({
-  imports: [MatSliderModule, MatSlideToggleModule],
-
-  exports: [MatSliderModule, MatSlideToggleModule],
-})
-export class MaterialModule {}
